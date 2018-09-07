d52af4a44d2b279dc2501423841d6557033d4e07
weiyd <weiyingda@lovefit.com>
2018-09-07 10:52:22 +0800

    项目初始

---
+++ MQ_WeTwitter/src/main/java/com/wetwitter/MqWeTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.wetwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqWeTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqWeTwitterApplication.class, args);
+	}
+}
